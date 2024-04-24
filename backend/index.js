3df1bee926a2cfaa0e3e680ad02d890de8e35fc9
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2024-04-24 13:51:25 +0530

    initial setup

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+//packages
+import path from 'path';
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import express from 'express'
+
+//utils
+import connectDB from './config/db.js'
+
+dotenv.config()
+const port = process.env.PORT || 5000
+
+connectDB();
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+    res.send('Hello world boss')
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port : ${port}`)
+})
+
+
